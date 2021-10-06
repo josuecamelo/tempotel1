09ddf273a98a863ddb89d1556806f288daa2dcb1
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2021-10-06 16:04:48 -0300

    Migrações de Produtos, Pedidos e Itens do Pedido

---
+++ database/migrations/2021_10_06_183650_create_pedidos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cliente_id')->constrained('clientes');
+            $table->decimal('valor_total',15,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+}
