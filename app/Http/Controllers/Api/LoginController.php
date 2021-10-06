039de8594acaf4d6fe964470db4318992ff2b536
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2021-10-06 15:31:19 -0300

    Login Usuário

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Admin;
+use Hash;
+use Validator;
+use Auth;
+
+class LoginController extends Controller
+{
+    public function userDashboard()
+    {
+        $users = User::all();
+        $success =  $users;
+
+        return response()->json($success, 200);
+    }
+
+    public function userLogin(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return response()->json(['error' => $validator->errors()->all()]);
+        }
+
+        if(auth()->guard('user')->attempt(['email' => request('email'), 'password' => request('password')])){
+
+            config(['auth.guards.api.provider' => 'user']);
+
+            $user = User::select('users.*')->find(auth()->guard('user')->user()->id);
+            $success =  $user;
+            $success['token'] =  $user->createToken('MyApp',['user'])->accessToken;
+
+            return response()->json($success, 200);
+        }else{
+            return response()->json(['error' => ['Email and Password are Wrong.']], 200);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,5 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('user/login',[LoginController::class, 'userLogin'])->name('userLogin');
+Route::group( ['prefix' => 'user','middleware' => ['auth:user-api','scopes:user'] ],function(){
+    // authenticated staff routes here
+    Route::get('dashboard',[LoginController::class, 'userDashboard']);
 });
 
