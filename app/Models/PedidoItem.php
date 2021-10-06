298ddd3e91343314b80b5a1a3c3048a0cd69b68d
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2021-10-06 18:00:53 -0300

    Model Pedido Item

--- app/Models/PedidoItem.php
+++ app/Models/PedidoItem.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PedidoItem extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'pedido_id', 'produto_id', 'quantidade', 'valor_unitario'
+    ];
+
+    public function produto(){
+       return $this->belongsTo(Produto::class);
+    }
 }
